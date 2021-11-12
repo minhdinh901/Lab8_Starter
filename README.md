da1f98079a29168d5a35920858990f1bff235dd0
Minh <m6dinh@ucsd.edu>
2021-11-12 00:48:10 -0800

    explore

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Check your understanding:
 
 1. I would put automated testing whenever someone attempts to merge code into the production branch. In other words, when a pull request into the production branch is opened or when new code is pushed to that pull request, then all the automated tests will run. However, I would also encourage people to run the tests in their local environment before pushing to the pull request to resolve testing issues beforehand.
 2. No, you would use a unit test.
+3. A messaging feature would require data to be sent across a network to another user. A unit test could simulate sending and receiving messages using mocks, but this would not be a very effective way to test a messaging features, so I would not use a unit test to test out messaging.
+4. Testing the max length of a message is very simple and isolated feature, therefore it would make sense to use a unit test to test it.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,83 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+test("phone number is empty", () => {
+  expect(functions.isPhoneNumber("")).toBeFalsy();
+});
+
+test("phone number is too short", () => {
+  expect(functions.isPhoneNumber("123-456")).toBeFalsy();
+});
+
+test("basic phone number", () => {
+  expect(functions.isPhoneNumber("123-4567")).toBeTruthy();
+});
+
+test("phone number with area code", () => {
+  expect(functions.isPhoneNumber("123-456-7890")).toBeTruthy();
+});
+
+test("email without @", () => {
+  expect(functions.isEmail("fakeemail.com")).toBeFalsy();
+});
+
+test("email without domain", () => {
+  expect(functions.isEmail("fake@")).toBeFalsy();
+});
+
+test("normal email", () => {
+  expect(functions.isEmail("fake@email.com")).toBeTruthy();
+});
+
+test("email with special characters", () => {
+  expect(functions.isEmail("f4k3@email.com")).toBeTruthy();
+});
+
+test("password is too short", () => {
+  expect(functions.isStrongPassword("abc")).toBeFalsy();
+});
+
+test("password does not start with letter", () => {
+  expect(functions.isStrongPassword("1bc")).toBeFalsy();
+});
+
+test("valid password with only letters", () => {
+  expect(functions.isStrongPassword("abcd")).toBeTruthy();
+});
+
+test("valid password with underscore", () => {
+  expect(functions.isStrongPassword("abcd_")).toBeTruthy();
+});
+
+test("date without year", () => {
+  expect(functions.isDate("01 / 01 / ")).toBeFalsy();
+});
+
+test("date with improper spacing", () => {
+  expect(functions.isDate("01 / 01 / 2000")).toBeFalsy();
+});
+
+test("date with double digit day and month", () => {
+  expect(functions.isDate("01/01/2000")).toBeTruthy();
+});
+
+test("date with single digit day and month", () => {
+  expect(functions.isDate("1/1/2000")).toBeTruthy();
+});
+
+test("hex color is too short", () => {
+  expect(functions.isHexColor("#f")).toBeFalsy();
+});
+
+test("hex color is too long", () => {
+  expect(functions.isHexColor("#fffffff")).toBeFalsy();
+});
+
+test("three symbol hex color", () => {
+  expect(functions.isHexColor("#fff")).toBeTruthy();
+});
+
+test("six symbol hex color", () => {
+  expect(functions.isHexColor("#ffffff")).toBeTruthy();
+});
